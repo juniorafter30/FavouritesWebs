ec8cfa2761520748468fe4be7e9e96a8a9c0a48e
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2021-04-15 10:57:51 +0200

    Moving forward

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <favourite-item
+      v-for="item in favourites"
+      :key="item.id"
+      :title="item.title"
+      :description="item.description"
+      :link="item.link"
+    ></favourite-item>
+  </ul>
+</template>
+
+<script>
+import FavouriteItem from './components/favourite-items/FavouriteItem';
+export default {
+  components: {
+    FavouriteItem
+  },
+  data() {
+    return {
+      favourites: [
+        {
+          id: 'facebook',
+          title: 'Facebook',
+          description: 'A place for you and your friends',
+          link: 'https://facebook.com'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'A place to search things, fast and easy',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/components/favourite-items/FavouriteItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link" target="_blank">Move me there!</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'link']
+};
+</script>
